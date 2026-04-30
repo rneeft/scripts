9f51376150ded3939b94fa26406a4fa83420b019
Rick Neeft <rick.neeft@outlook.com>
2026-04-30 23:15:43 +0200

    Create post-install.sh for development setup
    Add post-install script for setting up development environment

---
+++ ubuntu/post-install.sh
@@ -0,0 +1,10 @@
+git config --global user.name "Rick Neeft"
+git config --global user.email "rickneeft@outlook.com"
+
+curl -sSL https://scripts.rickneeft.dev/ubuntu/ohmyposh.sh | bash
+
+curl -sSL https://scripts.rickneeft.dev/ubuntu/install-godot.sh | bash
+
+curl -sSL https://scripts.rickneeft.dev/ubuntu/docker.sh | sudo bash
+
+curl -sSL https://aspire.dev/install.sh | bash
