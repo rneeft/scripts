695c6d28c4fd11db849d4bb28b7d7a878b45044a
Rick Neeft <rick.neeft@outlook.com>
2026-05-01 22:17:04 +0200

    Add Flatpak repository and install GNOME Boxes

--- ubuntu/post-install.sh
+++ ubuntu/post-install.sh
@@ -1,6 +1,9 @@
 git config --global user.name "Rick Neeft"
 git config --global user.email "rickneeft@outlook.com"
 
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.gnome.Boxes
+
 curl -sSL https://scripts.rickneeft.dev/ubuntu/ohmyposh.sh | bash
 
 curl -sSL https://scripts.rickneeft.dev/ubuntu/install-godot.sh | bash
